290fcfbcd799a2e6d028f0602c67b97acca06716
Kathrina Waugh <kathrinawaugh@kathrinas-mbp.myfiosgateway.com>
2020-10-02 19:46:40 -0400

    Flix Part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Kathrina Waugh on 10/1/20.
+//  Copyright © 2020 Kathrina. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -69,14 +69,34 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        
+        print("Loading up the details screen")
+        /*
+         This sends data from the moviesviewcontroller to the moviedetailsviewcontroller.
+         The sender is the tablecell.
+         TableView knows what the index is for a given cell
+         */
+        //Find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+        
+        //Pass the selected movie to the details view controller
+        
     }
-    */
 
 }
